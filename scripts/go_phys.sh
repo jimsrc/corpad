6b00b65c46183dd1abe68bf1b51a3aac36474ab3
jimsrc <jimmy.ilws@gmail.com>
2016-12-02 10:47:12 -0300

    extract the fitted-mean-free-path from each post.h5 file, and build a curve as function of r (heliodistance)

--- scripts/go_phys.sh
+++ scripts/go_phys.sh
@@ -3,7 +3,10 @@ F1=../out/h.001/post.h5
 F2=../out/h.002/post.h5
 F3=../out/h.003/post.h5
 F4=../out/h.004/post.h5
+F5=../out/h.005/post.h5
 
-./make_phys.py --pdb -- --LcSlab 0.01  --xi 1.0  --Bo 5e-5  --fnames $F1 $F2 $F3 $F4
+./make_phys.py --pdb -- --xi 1.0  \
+    --fnames $F1 $F2 $F3 $F4 $F5 \
+    --ro .2 .3 .5 .7 .9
 
 #EOF
